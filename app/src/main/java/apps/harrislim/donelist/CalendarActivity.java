980bad3c079fc3e9ebed834acc9a61615545c95f
harrislim <harris19921204@gmail.com>
2018-06-29 18:22:27 +0900

    Calendar 토,일,당일 색깔 변경

--- app/src/main/java/apps/harrislim/donelist/CalendarActivity.java
+++ app/src/main/java/apps/harrislim/donelist/CalendarActivity.java
@@ -5,9 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
 public class CalendarActivity extends AppCompatActivity {
     TextView ct;
     Intent i_self;
+    private final OneDayDecorator oneDayDecorator = new OneDayDecorator();
+    MaterialCalendarView materialCalendarView;
+
+    void customizeCalendar(){
+        materialCalendarView = (MaterialCalendarView)findViewById(R.id.calendarView);
+        materialCalendarView.addDecorators(
+                new SundayDecorator(),
+                new SaturdayDecorator(),
+                oneDayDecorator);
+        materialCalendarView.setSelectionMode(MaterialCalendarView.SELECTION_MODE_MULTIPLE);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +32,16 @@ public class CalendarActivity extends AppCompatActivity {
         i_self = getIntent();
         String title = i_self.getStringExtra("title");
         ct.setText(title);
+
+        customizeCalendar();
     }
 }
 
 /*
   사용한 오픈소스 캘린더 링크
   http://applandeo.com/blog/material-calendar-view-customized-calendar-widget-android/
+
+  이거 사용했어.
+  https://github.com/prolificinteractive/material-calendarview // github주소
+  http://dpdpwl.tistory.com/3 // 설명 블로그
 */
