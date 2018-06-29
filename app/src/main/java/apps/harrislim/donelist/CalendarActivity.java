049f627cf1cbf24dd1de74407bc093655ce03fd9
harrislim <harris19921204@gmail.com>
2018-06-29 17:33:47 +0900

    list완료, calendarView 진행중

--- app/src/main/java/apps/harrislim/donelist/CalendarActivity.java
+++ app/src/main/java/apps/harrislim/donelist/CalendarActivity.java
@@ -1,14 +1,22 @@
 package apps.harrislim.donelist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CalendarActivity extends AppCompatActivity {
-
+    TextView ct;
+    Intent i_self;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        ct = (TextView) findViewById(R.id.calendarTitle);
+        i_self = getIntent();
+        String title = i_self.getStringExtra("title");
+        ct.setText(title);
     }
 }
 
--- app/src/main/java/apps/harrislim/donelist/MainActivity.java
+++ app/src/main/java/apps/harrislim/donelist/MainActivity.java
@@ -1,27 +1,30 @@
 package apps.harrislim.donelist;
 
-import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ActivityChooserView;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import org.w3c.dom.Text;
+
+import java.util.*;
 
 import apps.harrislim.donelist.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
     private ImageButton plus, setting;
     Resources r;
     Button okBtn, cancelBtn;
@@ -32,18 +35,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     AlertDialog alert;
     android.widget.TextView topTV1;
     Intent inte;
+    ListView lv;
     ArrayList<Integer> viewIdList = new ArrayList<Integer>();
-    int viewId = 0;
-    boolean hasFinger = false;
-    boolean checkDelete = false;
-    int textViewId;
-    View innerView;
+    ArrayList<String> items = new ArrayList<String>();
+
+    boolean deletable = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         r = getResources();
+
+        // listview 관련
+        lv = (ListView)findViewById(R.id.listview);
+        lv.addFooterView(new View(this), null, true);
 
         plus = (ImageButton)findViewById(R.id.plus);
         setting = (ImageButton)findViewById(R.id.setting);
@@ -52,6 +58,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setting.setOnClickListener(this);
     }
 
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+        ArrayAdapter<String> aa = (ArrayAdapter<String>)parent.getAdapter();
+    }
+    ArrayAdapter<String> aa;
+    List list;
+    int positionIdx;
+    void m2(){
+        list = new Vector<String>();
+        for(String item: items) {
+            list.add(item);
+        }
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        lv.setAdapter(aa);
+
+        // textSize 변경하기 위한 코드
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                positionIdx = position;
+                if(deletable){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("알림");
+                    builder.setMessage("정말 삭제하시겠습니까?");
+                    DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (which == DialogInterface.BUTTON_POSITIVE) {
+                                String tempTitle = list.get(positionIdx).toString();
+                                Log.i("tag", "te: "+positionIdx);
+                                items.remove(positionIdx);
+                                list.remove(positionIdx);
+                                lv.setAdapter(aa);
+                                Toast.makeText(MainActivity.this,
+                                        tempTitle + "이(가) 삭제되었습니다.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                return;
+                            }
+                        }
+                    };
+                    builder.setPositiveButton("예", listener);
+                    builder.setNegativeButton("아니오", listener);
+                    builder.show();
+                }else {
+                    // 여기 달력 들어가는 곳.
+//                    Toast.makeText(MainActivity.this,
+//                            "선택(list) : " + aa.getItem(position), Toast.LENGTH_SHORT).show();
+                    inte = new Intent(MainActivity.this, CalendarActivity.class);
+                    inte.putExtra("title", aa.getItem(position));
+                    startActivity(inte);
+                }
+            }
+        });
+    }
     @Override
     public void onClick(View v){
         switch (v.getId()){
@@ -73,38 +133,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         addList = (EditText)view.findViewById(R.id.addListText);
         topLL = (android.widget.LinearLayout)findViewById(R.id.dynamicArea);
         topTV1 = new android.widget.TextView(MainActivity.this);
-
         inte = new Intent(MainActivity.this, CalendarActivity.class);
+
         topTV1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                String textViewIdStr = view.toString();
-                int lastIdx = textViewIdStr.lastIndexOf("}");
-                innerView = view;
-                textViewId = Integer.parseInt(textViewIdStr.substring(textViewIdStr.length()-2, lastIdx));
-                if(hasFinger) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setTitle("알림");
-                    builder.setMessage("정말 삭제하시겠습니까?");
-                    DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            if(which == DialogInterface.BUTTON_POSITIVE){
-                                Toast.makeText(MainActivity.this, "삭제가 완료되었습니다.", Toast.LENGTH_SHORT).show();
-
-                                topLL.removeView(innerView);
-                                viewIdList.set(textViewId, -1);
-                                checkDelete=true;
-                            }else{
-                                return;
-                            }
-                        }
-                    };
-                    builder.setPositiveButton("예", listener);
-                    builder.setNegativeButton("아니오", listener);
-                    builder.show();
-                }
-                else startActivity(inte);
+                  startActivity(inte);
             }
         });
 
@@ -113,26 +147,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             public void onClick(View view) {
                 String title = addList.getText().toString().trim();
-                if(title.length()>10) {
-                    Toast.makeText(MainActivity.this, "10글자를 초과할 수 없습니다.", Toast.LENGTH_LONG).show();
-                } else {
+                if(title.length()>20) {
+                    Toast.makeText(MainActivity.this, "20글자를 초과할 수 없습니다.", Toast.LENGTH_SHORT).show();
+                } else if(hasDuplicate(title)){ // list 중복검사
+                    Toast.makeText(MainActivity.this, "중복되는 목록이 있습니다.", Toast.LENGTH_SHORT).show();
+                }else {
                     Toast.makeText(MainActivity.this, "\""+title+"\" 추가", Toast.LENGTH_SHORT).show();
-                    viewIdList.add(viewId);
-                    topTV1.setId(viewIdList.get(viewId)); // id는 0부터 시작
-                    viewId++;
-                    topTV1.setText(title);
-                    topTV1.setTextSize(40);
-                    topTV1.setPadding(0, 32, 0, 0);
-                    topTV1.setBackground(MainActivity.this.getResources().getDrawable(R.drawable.envelope));
-                    android.widget.LinearLayout.LayoutParams lp = new android.widget.LinearLayout.LayoutParams(android.support.v7.widget.LinearLayoutCompat.LayoutParams.MATCH_PARENT, 150);
-
-                    lp.setMargins(20, 10, 20, 10);
-                    topTV1.setLayoutParams(lp);
-                    topTV1.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
                     if(emptyView!=null){
                         topLL.removeView(emptyView);
                      }
-                    topLL.addView(topTV1);
+                    items.add(title);
+                    m2();
                     alert.dismiss();
                 }
             }
@@ -145,28 +170,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
         alert.show();
     }
+    public boolean hasDuplicate(String name){
+        for(int i=0; i<items.size(); i++){
+            if(items.get(i).equals(name))
+                return true;
+        }
+        return false;
+    }
     public void setting(){
-        if(hasFinger){ // setting 비활성화
-             for(int i=0; i<viewIdList.size(); i++){
-                 if(viewIdList.get(i)!=-1) {
-                     android.widget.TextView tempTV = (android.widget.TextView) findViewById(i);
-                     tempTV.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
-                     tempTV.setPadding(0, 42, 0, 0);
-                 }
-            }
-        }else{ // setting 활성화
-             for(int i=0; i<viewIdList.size(); i++){
-                 if(viewIdList.get(i)!=-1) {
-                     android.widget.TextView tempTV = (android.widget.TextView) findViewById(i);
-                     tempTV.setCompoundDrawablesWithIntrinsicBounds(R.drawable.trash_bin, 0, R.drawable.finger, 0);
-                     tempTV.setPadding(10, 42, 20, 42);
-                 }
-            }
+        if(deletable){ // 지우기 멈춤
+            Toast.makeText(this, "삭제 비활성화", Toast.LENGTH_SHORT).show();
+            setting.setImageResource(R.drawable.setting);
+        }else{ // 지우기 시작
+            Toast.makeText(this, "삭제 활성화", Toast.LENGTH_SHORT).show();
+            setting.setImageResource(R.drawable.unsetting);
         }
         settingToggle();
     }
     void settingToggle(){
-        if(hasFinger) hasFinger=false;
-        else hasFinger=true;
+        if(deletable) deletable=false;
+        else deletable=true;
     }
 }
+
